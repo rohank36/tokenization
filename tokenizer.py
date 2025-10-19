34fdb0f6e6d86b4e3fd380ecbeebeff589e22b01
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-10-18 23:37:33 -0400

    first commit

---
+++ tokenizer.py
@@ -0,0 +1,14 @@
+
+class BasicTokenizer:
+
+    def train(self,text,vocab_size,verbose=False):
+        pass
+
+    def encode(self,text):
+        pass
+
+    def decode(self,tokens):
+        pass
+
+if __name__ == "__main__":
+    print("hello world")
